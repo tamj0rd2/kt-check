19ac9c859126acb244efc7b33143246f55822c3e
Tamara Jordan <20561445+tamj0rd2@users.noreply.github.com>
2025-12-20 17:43:26 +0000

    Implement filter generators

--- src/main/kotlin/com/tamj0rd2/ktcheck/genv2/Generators.kt
+++ src/main/kotlin/com/tamj0rd2/ktcheck/genv2/Generators.kt
@@ -1,9 +1,12 @@
 package com.tamj0rd2.ktcheck.genv2
 
 import com.tamj0rd2.ktcheck.gen.OneOfEmpty
-import kotlin.reflect.KClass
 
 fun <T> Gen.Companion.constant(value: T): Gen<T> = sample().map { value }
+
+// todo:does this distribute appropriately?
+fun Gen.Companion.boolean(): Gen<Boolean> =
+    sample().map { sample -> (sample.value and 1UL) == 1UL }
 
 fun <T> Gen.Companion.oneOf(gens: List<Gen<T>>): Gen<T> {
     if (gens.isEmpty()) throw OneOfEmpty()
@@ -19,22 +22,4 @@ fun <T> Gen.Companion.of(values: Collection<T>): Gen<T> = Gen.oneOf(values.map(:
 @JvmName("ofComparable")
         /** Shrinks toward the smallest value */
 fun <T : Comparable<T>> Gen.Companion.of(values: Collection<T>): Gen<T> = Gen.oneOf(values.sorted().map(::constant))
-
-@Suppress("unused")
-fun <T> Gen<T>.filter(predicate: (T) -> Boolean): Gen<T> = filter(100, predicate)
-
-@Suppress("unused", "UnusedReceiverParameter")
-fun <T> Gen<T>.filter(threshold: Int, predicate: (T) -> Boolean): Gen<T> = TODO("Not yet implemented")
-
-@Suppress("unused")
-class FilterLimitReached(threshold: Int) :
-    IllegalStateException("Gen.filter() exceeded the threshold of misses")
-
-@Suppress("unused", "UnusedReceiverParameter")
-fun <T> Gen<T>.ignoreExceptions(klass: KClass<out Throwable>, threshold: Int = 100): Gen<T> =
-    TODO("Not yet implemented")
-
-@Suppress("unused")
-class ExceptionLimitReached(threshold: Int, override val cause: Throwable) :
-    IllegalStateException("Gen.ignoreExceptions() exceeded the threshold of $threshold exceptions")
 
