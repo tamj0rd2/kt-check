b0546231f19d2b8540a4a5495771b44d8099e455
Tamara Jordan <20561445+tamj0rd2@users.noreply.github.com>
2026-04-03 15:44:16 +0100

    Tidy up contract

--- src/test/kotlin/com/tamj0rd2/ktcheck/contracts/TestFrameworkContract.kt
+++ src/test/kotlin/com/tamj0rd2/ktcheck/contracts/TestFrameworkContract.kt
@@ -9,60 +9,51 @@ import org.junit.jupiter.api.Test
 import strikt.api.expectDoesNotThrow
 import strikt.api.expectThat
 import strikt.api.expectThrows
+import strikt.assertions.cause
 import strikt.assertions.containsExactlyInAnyOrder
 import strikt.assertions.isEqualTo
 import strikt.assertions.isNotNull
-import java.io.ByteArrayOutputStream
+import strikt.assertions.isNull
 
 internal interface TestFrameworkContract : BaseContract {
     override val exampleGen get() = null
 
     @Test
-    fun `forAll does not throw if the property holds true`() {
-        val outputStream = ByteArrayOutputStream()
-        val testConfig = TestConfig().withReportingStream(outputStream)
-
-        expectDoesNotThrow { forAll(testConfig, constant(true)) { it } }
+    fun `does not throw if the property is not falsified`() {
+        expectDoesNotThrow { forAll(int()) { true } }
+        expectDoesNotThrow { checkAll(int()) { } }
     }
 
     @Test
-    fun `forAll throws if the property is falsified`() {
-        expectThrows<PropertyFalsifiedException> { forAll(constant(false)) { it } }
+    fun `throws if the property is falsified`() {
+        expectThrows<PropertyFalsifiedException> { forAll(int()) { false } }.cause.isNull()
+
+        val expectedError = AssertionError("bad")
+        expectThrows<PropertyFalsifiedException> { checkAll(int()) { throw expectedError } }
+            .cause.isEqualTo(expectedError)
     }
 
     @Test
-    fun `forAll rethrows unexpected exceptions that occur`() {
-        class MyThrowable : Throwable()
-        val throwable = MyThrowable()
-
-        expectThrows<MyThrowable> { forAll(constant(throwable)) { throw it } }.isEqualTo(throwable)
-    }
-
-    @Test
-    fun `checkAll does not throw if the property doesn't throw`() {
-        checkAll(constant(null)) { }
-
-        expectDoesNotThrow { forAll(constant(true)) { it } }
-    }
-
-    @Test
-    fun `checkAll rethrows unexpected exceptions that occur`() {
+    fun `rethrows unexpected exceptions that occur`() {
         class MyThrowable : Throwable()
 
         val throwable = MyThrowable()
-        expectThrows<MyThrowable> { checkAll(constant(throwable)) { throw it } }.isEqualTo(throwable)
+        expectThrows<MyThrowable> { forAll(int()) { throw throwable } }.isEqualTo(throwable)
+        expectThrows<MyThrowable> { checkAll(int()) { throw throwable } }.isEqualTo(throwable)
     }
 
+    // todo: write another test to prove it includes user provided edge cases.
     @Test
     fun `includes edge cases during test iterations`() {
         val seenValues = mutableSetOf<Int>()
-
-        forAll(int()) {
-            seenValues.add(it)
-            true
-        }
-
+        val gen = int()
         val expectedEdges = setOf(0, 1, -1, Int.MIN_VALUE, Int.MIN_VALUE + 1, Int.MAX_VALUE, Int.MAX_VALUE - 1)
+
+        forAll(gen) { seenValues.add(it); true }
+        expectThat(seenValues.take(expectedEdges.size)).containsExactlyInAnyOrder(expectedEdges)
+
+        seenValues.clear()
+        checkAll(gen) { seenValues.add(it) }
         expectThat(seenValues.take(expectedEdges.size)).containsExactlyInAnyOrder(expectedEdges)
     }
 
@@ -73,14 +64,12 @@ internal interface TestFrameworkContract : BaseContract {
         val iterationToCheck = (1..100).random()
         val gen = int()
 
-
         var iterationCount = 0
         var valueOnSpecifiedIteration: Int? = null
 
-        forAll(initialConfig, gen) {
+        checkAll(initialConfig, gen) {
             iterationCount++
             if (iterationCount == iterationToCheck) valueOnSpecifiedIteration = it
-            true
         }
 
         expectThat(iterationCount).isEqualTo(initialConfig.iterations)
@@ -90,10 +79,9 @@ internal interface TestFrameworkContract : BaseContract {
         var valueOnRetry: Int? = null
         val replayConfig = initialConfig.replay(initialConfig.seed.value, iterationToCheck)
 
-        forAll(replayConfig, gen) {
+        checkAll(replayConfig, gen) {
             replayedIterations++
             valueOnRetry = it
-            true
         }
 
         expectThat(replayedIterations).isEqualTo(1)
