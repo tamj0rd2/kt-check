8ef68ab0a6f40328ea903890e27d75ddad145690
Tamara Jordan <20561445+tamj0rd2@users.noreply.github.com>
2026-04-05 13:44:53 +0100

    Extract class

---
+++ src/test/kotlin/com/tamj0rd2/ktcheck/incubating/IncubatingBaseContractImpl.kt
@@ -0,0 +1,24 @@
+package com.tamj0rd2.ktcheck.incubating
+
+import com.tamj0rd2.ktcheck.GenBuilders
+import com.tamj0rd2.ktcheck.contracts.BaseContract
+import com.tamj0rd2.ktcheck.contracts.GenResults
+import com.tamj0rd2.ktcheck.core.GenerationContext
+import com.tamj0rd2.ktcheck.core.Seed
+
+internal abstract class IncubatingBaseContractImpl : BaseContract, GenBuilders by Gen.Companion {
+    override fun ctx(seed: Seed): GenerationContext = GenContext.new(seed)
+
+    override fun <T> com.tamj0rd2.ktcheck.Gen<T>.generate(ctx: GenerationContext): GenResults<T> {
+        return buildGenResults((this as Gen).generate(ctx as GenContext))
+    }
+
+    override fun <T> com.tamj0rd2.ktcheck.Gen<T>.edgeCase(seed: Seed): GenResults<T> {
+        return generate(GenContext.new(seed, ShouldGenerateEdgeCase.Always))
+    }
+
+    private fun <T> buildGenResults(result: GenResult<T>): GenResults<T> = GenResults(
+        value = result.value,
+        shrinks = result.shrinks.map { buildGenResults(it) }
+    )
+}
