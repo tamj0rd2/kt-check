b5e4f030b55cd39d7e5810eec0d6fc328d8f30f0
Tamara Jordan <20561445+tamj0rd2@users.noreply.github.com>
2026-04-05 18:11:03 +0100

    Implement distinct list gen

--- src/main/kotlin/com/tamj0rd2/ktcheck/incubating/ListGen.kt
+++ src/main/kotlin/com/tamj0rd2/ktcheck/incubating/ListGen.kt
@@ -1,26 +1,27 @@
 package com.tamj0rd2.ktcheck.incubating
 
 import com.tamj0rd2.ktcheck.GenerationException
+import dev.forkhandles.result4k.Result
 import dev.forkhandles.result4k.Result4k
+import dev.forkhandles.result4k.asFailure
 import dev.forkhandles.result4k.asSuccess
 import dev.forkhandles.result4k.onFailure
 
-internal data class ListGen<T>(
-    private val gen: Gen<T>,
-    private val sizeRange: IntRange,
-) : GenProvider<List<T>> {
-    override fun generate(ctx: GenContext): Result4k<GeneratedValue<List<T>>, GenerationException> {
-        val size = Gen.int(sizeRange).generate(ctx.left).onFailure { return it }
-        val elements = buildList {
-            var ctx = ctx.right
-            repeat(size.value) {
-                add(gen.generate(ctx.left).onFailure { return it })
-                ctx = ctx.right
-            }
-        }
+internal sealed class BaseListGen<T> : GenProvider<List<T>> {
+    protected abstract val sizeRange: IntRange
 
+    final override fun generate(ctx: GenContext): Result4k<GeneratedValue<List<T>>, GenerationException> {
+        val size = Gen.int(sizeRange).generate(ctx.left).onFailure { return it }
+        val elements = generateElements(ctx.right, size.value).onFailure { return it }
         return buildResult(size, elements).asSuccess()
     }
+
+    protected abstract fun generateElements(
+        ctx: GenContext,
+        targetSize: Int,
+    ): Result<List<GeneratedValue<T>>, GenerationException>
+
+    protected open fun List<GeneratedValue<T>>.isValid(targetSize: Int): Boolean = true
 
     private fun buildResult(
         size: GeneratedValue<Int>,
@@ -34,9 +35,12 @@ internal data class ListGen<T>(
         }
 
         val elementBasedShrinks = elements.indices.asSequence().flatMap { index ->
-            elements[index].shrinks.map { shrunkElement ->
-                val updatedElements = elements.toMutableList().apply { set(index, shrunkElement) }
-                buildResult(size, updatedElements)
+            elements[index].shrinks.mapNotNull { shrunkElement ->
+                elements
+                    .toMutableList()
+                    .apply { set(index, shrunkElement) }
+                    .takeIf { it.isValid(size.value) }
+                    ?.let { buildResult(size, it) }
             }
         }
 
@@ -44,5 +48,60 @@ internal data class ListGen<T>(
             value = elements.map { it.value },
             shrinks = sizeBasedShrinks + elementBasedShrinks,
         )
+    }
+}
+
+internal data class ListGen<T>(
+    private val gen: Gen<T>,
+    override val sizeRange: IntRange,
+) : BaseListGen<T>() {
+    override fun generateElements(
+        ctx: GenContext,
+        targetSize: Int,
+    ): Result<List<GeneratedValue<T>>, GenerationException> =
+        buildList {
+            var ctx = ctx
+            repeat(targetSize) {
+                add(gen.generate(ctx.left).onFailure { return it })
+                ctx = ctx.right
+            }
+        }.asSuccess()
+}
+
+internal data class DistinctListGen<T>(
+    private val gen: Gen<T>,
+    override val sizeRange: IntRange,
+) : BaseListGen<T>() {
+    override fun List<GeneratedValue<T>>.isValid(targetSize: Int) = distinctBy { it.value }.size == targetSize
+
+    override fun generateElements(
+        ctx: GenContext,
+        targetSize: Int,
+    ): Result<List<GeneratedValue<T>>, GenerationException> = buildList {
+        var ctx = ctx
+        var attempts = 0
+        val seenValues = mutableSetOf<T>()
+        while (size < targetSize) {
+            if (attempts > MAX_ATTEMPTS_PER_ELEMENT) {
+                return GenerationException.DistinctCollectionSizeImpossible(
+                    minSize = targetSize,
+                    achievedSize = size,
+                    attempts = attempts,
+                ).asFailure()
+            }
+
+            attempts += 1
+            val element = gen.generate(ctx.left).onFailure { return it }
+            if (seenValues.add(element.value)) {
+                add(element)
+                attempts = 0
+            }
+
+            ctx = ctx.right
+        }
+    }.asSuccess()
+
+    private companion object {
+        const val MAX_ATTEMPTS_PER_ELEMENT = 100
     }
 }
